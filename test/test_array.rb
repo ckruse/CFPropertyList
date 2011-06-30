8918cd1265e21bc2868499938818aade7b896b8c
Greg Weber <greg@gregweber.info>
2011-06-29 23:28:46 -0700

    renameing

--- test/test_array.rb
+++ test/test_array.rb
@@ -30,20 +30,12 @@ class TestArray < Test::Unit::TestCase
 
   def test_big_array
     require 'mongo'
-    arr = Marshal.load File.read('test/reference/yap_schedules.rb')
+    arr = Marshal.load File.read('test/reference/big_array.rb')
     plist = CFPropertyList::List.new
     plist.value = CFPropertyList.guess(
       arr, :converter_method => :to_plist_item,:convert_unknown_to_string => true
     )
     assert_equal raw_xml('big_array'), plist.to_str(CFPropertyList::List::FORMAT_XML, :formatted => false)
     assert_equal raw_binary('big_array'), plist.to_str(CFPropertyList::List::FORMAT_BINARY)
-
-    # create files
-    #File.open('big_array.xml', 'w') {|fh|
-      #fh.write plist.to_str(CFPropertyList::List::FORMAT_XML, :formatted => false)
-    #}
-    #File.open('big_array.plist', 'w') {|fh|
-      #fh.write plist.to_str(CFPropertyList::List::FORMAT_BINARY)
-    #}
   end
 end

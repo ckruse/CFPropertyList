cbe76b6f4065f37be3394fead5e7174a4dbb1992
Greg Weber <greg@gregweber.info>
2011-06-29 17:40:23 -0700

    add big_array test

--- test/test_array.rb
+++ test/test_array.rb
@@ -20,4 +20,30 @@ class TestArray < Test::Unit::TestCase
     assert_equal raw_xml('array'), plist.to_str(CFPropertyList::List::FORMAT_XML, :formatted => false)
     assert_equal raw_binary('array'), plist.to_str(CFPropertyList::List::FORMAT_BINARY)
   end
+
+  def test_write_enumerator
+    plist = CFPropertyList::List.new
+    plist.value = CFPropertyList.guess([ "object" ].to_enum)
+    assert_equal raw_xml('array'), plist.to_str(CFPropertyList::List::FORMAT_XML, :formatted => false)
+    assert_equal raw_binary('array'), plist.to_str(CFPropertyList::List::FORMAT_BINARY)
+  end
+
+  def test_big_array
+    require 'mongo'
+    arr = Marshal.load File.read('test/reference/yap_schedules.rb')
+    plist = CFPropertyList::List.new
+    plist.value = CFPropertyList.guess(
+      arr, :converter_method => :to_plist_item,:convert_unknown_to_string => true
+    )
+    assert_equal raw_xml('big_array'), plist.to_str(CFPropertyList::List::FORMAT_XML, :formatted => false)
+    assert_equal raw_binary('big_array'), plist.to_str(CFPropertyList::List::FORMAT_BINARY)
+
+    # create files
+    #File.open('big_array.xml', 'w') {|fh|
+      #fh.write plist.to_str(CFPropertyList::List::FORMAT_XML, :formatted => false)
+    #}
+    #File.open('big_array.plist', 'w') {|fh|
+      #fh.write plist.to_str(CFPropertyList::List::FORMAT_BINARY)
+    #}
+  end
 end

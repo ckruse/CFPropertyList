b5689697777fa0d9f4bb7b7bc9198be0e2509be0
Shubham Pathak <shubham.p1994@gmail.com>
2019-12-06 12:40:21 +0530

    refactor: remove NOENT option for security NOENT, which is used to substitute entities also enables processing of both regular and external entities. Which seems risky when you don't the XML source. NONET will have no effect if used with NOENT.
    - Github Issue: https://github.com/ckruse/CFPropertyList/issues/55

--- lib/cfpropertylist/rbNokogiriParser.rb
+++ lib/cfpropertylist/rbNokogiriParser.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 module CFPropertyList
   # XML parser
   class NokogiriXMLParser < ParserInterface
-    PARSER_OPTIONS = Nokogiri::XML::ParseOptions::NOBLANKS|Nokogiri::XML::ParseOptions::NOENT|Nokogiri::XML::ParseOptions::NONET
+    PARSER_OPTIONS = Nokogiri::XML::ParseOptions::NOBLANKS|Nokogiri::XML::ParseOptions::NONET
     # read a XML file
     # opts::
     # * :file - The filename of the file to load
